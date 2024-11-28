98179153dfb797d5cc3603d84910c473ac17dfe8
NunesAzevedo <pnazevedo1@gmail.com>
2024-11-27 22:53:01 -0300

    Desenvolvimento inicial da main do projeto

--- src/main.c
+++ src/main.c
@@ -8,9 +8,97 @@
 #include "arvore_bin.h"
 #include "circuitos.h"
 #include "common.h"
+#include "main.h"
 
 int main()
 {
-    
+    // Árvore binária que representa o circuito lógico
+    ArvoreBin *circuito;
+    if (circuito == NULL)
+        printaErro("Falha ao alocar a árvore 'brain'");
+
+    FILE *brain = fopen("brain.txt", "rt");
+    if (brain == NULL)
+        printaErro("Falha ao abrir o arquivo: brain");
+
+    // Devido ao padrão do arquivo, de primeiro ter um número, e em seguida,
+    // caso não seja '-1', ser uma string, basta verificar se esse número
+    // não é o '-1', e se for o caso, proceder com a leitura do arquivo
+
+    int id;
+    char *tipo_nodo;
+    while (fscanf(brain, "%d", &id) == 1)
+    {
+        if (DEBUGGING)
+            printf("\n[DEBUG] Lendo arquivo da arvore.");
+
+        if (id == -1) // Encerra a leitura do arquivo
+        {
+            if (DEBUGGING)
+                printf("\n[DEBUG]: id = -1 (quebra o loop), valor da id: %d", id);
+            break;
+        }
+
+        if (fscanf(brain, "%s", tipo_nodo) != 1)
+        {
+            printaErro("Erro ao tentar ler string relativa à última ID");
+        }
+
+        if (DEBUGGING)
+            printf("\n[DEBUG]: Tipo de nodo lido no arquivo: %s", tipo_nodo);
+
+        if ((strcmp(tipo_nodo, AND2) == 0) ||
+            (strcmp(tipo_nodo, OR_2) == 0) ||
+            (strcmp(tipo_nodo, XOR_2) == 0) ||
+            (strcmp(tipo_nodo, NOT2) == 0) ||
+            (strcmp(tipo_nodo, INP1) == 0))
+        {
+            if (DEBUGGING)
+            {
+                printaErro("String lida eh invalida.");
+            }
+            else
+            {
+                printaFalha();
+            }
+        }
+
+        insereArvoreBin(circuito, id, tipo_nodo);
+    }
+    free(tipo_nodo);
+    fclose(brain);
+
+    FILE *input = fopen("input.txt", "rt");
+    if (input == NULL)
+        printaErro("Falha ao abrir o arquivo: input");
+
+    float valor;
+    while (1)
+    {
+        if (DEBUGGING)
+            printf("\n[DEBUG] Lendo arquivo com os inputs.");
+
+        if (fscanf(input, "%f", valor) != 1)
+        {
+            if (DEBUGGING)
+            {
+                printf("\n[DEBUG]: valor lido nao eh uma entrada valida. Valor lido de 'valor': %d", valor);
+            }
+            else
+            {
+                printaFalha();
+            }
+        }
+
+        if (valor == -1)
+        {
+            if (DEBUGGING)
+                printf("\n[DEBUG]: Valor de '-1' lido, fim do loop");
+            break;
+        }
+
+        
+    }
+
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -8,7 +8,7 @@
 #define MAIN_H
 
 // *************************************
-// Variável que diz se será mostrado ou 
+// Variável que diz se será mostrado ou
 // não os prints de debugging do projeto
 // 1: Será mostrado
 // 0: Não será mostrado
@@ -17,12 +17,12 @@
 #define DEBUGGING 1
 // *************************************
 
-
-// ID's possíveis
-#define AND2 "AND2";
-#define OR_2 "OR-2";
-#define XOR_2 "XOR2";
-#define NOT2 "NOT2";
-#define INP1 "INP1";
+// Tipos de Nodo
+const int tam_tipo_nodo = 5; // 4 letras + '\0'
+const char AND2[tam_tipo_nodo] = "AND2";
+const char OR_2[tam_tipo_nodo] = "OR-2";
+const char XOR_2[tam_tipo_nodo] = "XOR2";
+const char NOT2[tam_tipo_nodo] = "NOT2";
+const char INP1[tam_tipo_nodo] = "INP1";
 
 #endif // MAIN_H
